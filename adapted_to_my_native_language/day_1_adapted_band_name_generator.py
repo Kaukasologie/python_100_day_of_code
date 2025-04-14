ba7d5ec4a9478fda70049dbd1691cdce7373d81e
Caucasology <akhmad.gazimuhammadil@utlook.com>
2025-04-15 00:47:47 +0200

    Къокъаялъе цlар ургъизе кумек гьабулеб программа

---
+++ adapted_to_my_native_language/day_1_adapted_band_name_generator.py
@@ -0,0 +1,6 @@
+# Generates a name for your band by combining the name of the city you grew up in with the name of your first pet
+
+print("\nВорчlами къокъаялъе цlар ургъизе кумек гьабулеб программаялде!")
+city = input("Мун гlурав(-й) росуда яги шагьаралда цlар щиб?\n")
+pet = input("Дур тlоцебесеб хlайваналда цlар щиб букlараб?\n")
+print("Дур къокъаялда цlар букlине бегьула " + city + "са " + pet)
