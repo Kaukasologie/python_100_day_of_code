3c0524c2d328c416baf4d90ff56e3d17ae8d3072
Caucasology <akhmad.gazimuhammadil@outlook.com>
2025-04-14 22:06:26 +0200

    Added comments explaining what the program does

--- day_1_to_14_mini_projects/day_1_band_name_generator.py
+++ day_1_to_14_mini_projects/day_1_band_name_generator.py
@@ -1,3 +1,5 @@
+# Generates a name for your band by combining the name of the city you grew up in with the name of your first pet
+
 print("Welcome to the Band Name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
