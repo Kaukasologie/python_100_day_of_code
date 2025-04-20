367961154bccc1ea32ff4a08ffd6b769f7d98761
Caucasology <akhmad.gazimuhammadil@outlook.com>
2025-04-21 00:08:03 +0200

    Calculator version 1.2 (add exceptions)

--- day_1_to_14_mini_projects/day_10_2_calculator.py
+++ day_1_to_14_mini_projects/day_10_2_calculator.py
@@ -20,19 +20,35 @@ operations= {
 }
 
 while True:
-    first_number = float(input("What's the first number?: "))
-    continue_calculating = True
+    try:
+        print("\nNew calculation.")
+        first_number = float(input("What's the first number?:\n--->> "))
+    except ValueError:
+        print("Wrong input. I only work with numbers. Please enter a number.")
+    else:
+        is_continue_calculating = True
+        while is_continue_calculating:
+            selected_operation = input("Pick an operation:\n+\n-\n*\n/\n--->> ")
+            while selected_operation not in "+-*/":
+                print('This is not operation or is not supported. Pick an operation from list below or "exit": "+", "-", "*" or "/"')
+                selected_operation = input("Pick a supported operation:\n--->> ")
 
-    while continue_calculating:
-        selected_operation = input("+\n-\n*\n/\nPick an operation: ")
-        next_number = float(input("What's the next number?: "))
-
-        for key in operations:
-            if selected_operation == key:
-                result = operations[key](first_number,next_number)
-                print(f"{first_number} {selected_operation} {next_number} = {result}")
-                if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower() == 'n':
-                    continue_calculating = False
-                    print("\n" * 3)
+            is_correct_number = False
+            while not is_correct_number:
+                try:
+                    next_number = float(input("What's the next number?: "))
+                except ValueError:
+                    print("Wrong input 2. I only work with numbers. Please enter a number.")
                 else:
-                    first_number = result
+                    is_correct_number = True
+                    result = operations[selected_operation](first_number, next_number)
+                    print(f"{first_number} {selected_operation} {next_number} = {result}")
+                    continue_or_not = input(f"Type 'yes' to continue calculating with {result}, or type 'no' to start a new calculation:\n--->> ").lower()
+                    if continue_or_not == "yes":
+                        first_number = result
+                    elif continue_or_not == "no":
+                        print("\n" * 3)
+                        is_continue_calculating = False
+                    else:
+                        print('Wrong input. You had to enter "yes" or "no"')
+                        is_continue_calculating = False
