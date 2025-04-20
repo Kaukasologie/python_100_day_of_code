81b185c1dc2a83dba9b1337078f91d188ec4f43a
Caucasology <akhmad.gazimuhammadil@outlook.com>
2025-04-21 00:04:52 +0200

    Calculator version 1.1 (dictionaries solution)

--- day_1_to_14_mini_projects/day_10_2_calculator.py
+++ day_1_to_14_mini_projects/day_10_2_calculator.py
@@ -1,4 +1,4 @@
-# Simply calculator
+# Simple calculator solution through dictionaries
 
 def add(n1, n2):
     return n1 + n2
@@ -12,33 +12,27 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+operations= {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
 while True:
     first_number = float(input("What's the first number?: "))
     continue_calculating = True
 
     while continue_calculating:
-        operation = input("+\n-\n*\n/\nPick an operation: ")
+        selected_operation = input("+\n-\n*\n/\nPick an operation: ")
         next_number = float(input("What's the next number?: "))
-        result = ''
 
-        if operation == "+":
-            result = add(first_number, next_number)
-        elif operation == "-":
-            result = subtract(first_number, next_number)
-        elif operation == "*":
-            result = multiply(first_number, next_number)
-        elif operation == "/":
-            result = divide(first_number, next_number)
-        else:
-            print("Operator not correct")
-            continue_calculating = False
-
-        if type(result) == float:
-            print(f"{first_number} {operation} {next_number} = {result}")
-
-            if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower() == 'n':
-                continue_calculating = False
-            else:
-                first_number = result
-
-    print("\n" * 3)
+        for key in operations:
+            if selected_operation == key:
+                result = operations[key](first_number,next_number)
+                print(f"{first_number} {selected_operation} {next_number} = {result}")
+                if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower() == 'n':
+                    continue_calculating = False
+                    print("\n" * 3)
+                else:
+                    first_number = result
