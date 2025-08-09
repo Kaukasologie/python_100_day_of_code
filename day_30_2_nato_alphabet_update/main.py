cb15c0662ab87e7e018832687ddc430e84d7ff82
Caucasology <akhmad.gazimuhammadil@outlook.com>
2025-08-09 23:27:13 +0200

    Nato Alphabet Update

---
+++ day_30_2_nato_alphabet_update/main.py
@@ -0,0 +1,19 @@
+#TODO: does not recognize space
+
+import pandas
+
+data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_alphabet_dict = {row.letter: row.code for (index, row) in data_frame.iterrows()}
+
+def generate_phonetic():
+    users_word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_alphabet_dict[letter] for letter in users_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
